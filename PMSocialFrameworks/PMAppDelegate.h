88365cc6a3bcd07f74e5cc28285a368c18930863
Paola Mata Maldonado <pmatanyc@gmail.com>
2014-07-22 14:11:13 -0400

    Initial Commit

---
+++ PMSocialFrameworks/PMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PMAppDelegate.h
+//  PMSocialFrameworks
+//
+//  Created by Paola Mata Maldonado on 7/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PMSocialFrameworks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PMSocialFrameworks
+//
+//  Created by Paola Mata Maldonado on 7/22/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PMAppDelegate class]));
+    }
+}
